a4d7a2358e97c40931ee4439d04fe221df3a7a2a
13949836783@163.com <13949836783@163.com>
2017-08-01 22:38:56 +0800

    add post model and controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,22 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all
+  end
+
   def new
     @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+
+    @post.create(params[:post].permit(:title, :content))
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
 end
