6b9c196abc554b0445b8eded4893fe788551d1a9
13949836783@163.com <13949836783@163.com>
2017-08-02 11:09:39 +0800

    user can create post and see the post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,28 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order('created_at DESC')
   end
 
   def new
-    @post = Post.new
+    
   end
 
   def create
-    @post = Post.new
+    @post = Post.new(post_params)
 
-    @post.create(params[:post].permit(:title, :content))
+    @post.save
     redirect_to @post
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 
 end
