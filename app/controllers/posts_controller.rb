8ffa4497d40b37541b43a2f50428b4ec2caa2ca5
13949836783@163.com <13949836783@163.com>
2017-08-02 15:58:51 +0800

    user can edit and delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,24 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else
+      render "edit"
+    end
+  end
+
   def create
     @post = Post.new(post_params)
 
@@ -18,10 +36,12 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
+  def destroy
     @post = Post.find(params[:id])
-  end
 
+    @post.destroy
+    redirect_to posts_path
+  end
 
   private
 
